7446691ce4f7f143f9b53e8abc884c6de7f6403a
Luca S.G. de Marinis <loop23@gmail.com>
2012-08-31 18:02:23 +0200

    Added sample and some methods

--- lib/ttml.rb
+++ lib/ttml.rb
@@ -2,9 +2,27 @@ require "ttml/version"
 require "nokogiri"
 
 module Ttml
-  class Document < Nokogiri::Document
-    def subtitle_stream
-      raise "Unimplemented!"
+  class Document < Nokogiri::XML
+    # Returns the subtitle from "from" to "to" as an enumerator
+    # (or all subtitles if both are missing)
+    def subtitle_stream from = 0.0, to = 99999999.0
+      xpath "//p"
     end
+
+    # Returns document title
+    def title
+      xpath "//ns2::title".text
+    end
+
+    # Returns document description
+    def title
+      xpath "//ns2::description".text
+    end
+
+    # Returns document copyright
+    def copyright
+      xpath "//ns2:copyright".text
+    end
+
   end
 end
