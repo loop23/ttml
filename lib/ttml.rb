970ba8241f1b5e27fb23b708971560b9244c9cb2
Luca S.G. de Marinis <loop23@gmail.com>
2012-09-03 15:00:02 +0200

    First usable version
    Add tests, implement core functionality

--- lib/ttml.rb
+++ lib/ttml.rb
@@ -2,26 +2,39 @@ require "ttml/version"
 require "nokogiri"
 
 module Ttml
-  class Document < Nokogiri::XML
-    # Returns the subtitle from "from" to "to" as an enumerator
-    # (or all subtitles if both are missing)
+  class Document
+
+    attr_reader :doc
+
+    def initialize file_or_stream
+      stream = file_or_stream.is_a?(IO) ? file_or_stream : File.open(file_or_stream)
+      @doc = Nokogiri::XML(stream)
+    end
+
+    # Returns subtitles from "from" to "to" as an array
+    # (or all subtitles if both are missing).
+    # I tried using xpath functions, without success,
+    # as in xmlns:div/xmlns:p[number(@begin)=>746.63] - any ideas?
     def subtitle_stream from = 0.0, to = 99999999.0
-      xpath "//p"
+      doc.xpath("/xmlns:tt/xmlns:body/xmlns:div/xmlns:p").select {|n|
+        # puts "Vedo se #{ n['begin'].to_f } >= #{ from } e se #{ n['end'].to_f } <= #{ to }"
+        (n['begin'].to_f >= from) && (n['end'].to_f <= to)
+      }
     end
 
     # Returns document title
     def title
-      xpath "//ns2::title".text
+      doc.xpath("//ns2:title")[0].children[0].content
     end
 
     # Returns document description
-    def title
-      xpath "//ns2::description".text
+    def description
+      doc.xpath("//ns2:description")[0].children[0].content
     end
 
     # Returns document copyright
     def copyright
-      xpath "//ns2:copyright".text
+      doc.xpath("//ns2:copyright")[0].children[0].content
     end
 
   end
---
+++ test/test_ttml.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require 'ttml'
+
+class TtmlTest < Test::Unit::TestCase
+
+  def setup
+    @doc = Ttml::Document.new(File.join(File.dirname(__FILE__), 'sample.xml'))
+  end
+
+  def test_class
+    assert @doc.is_a?(Ttml::Document)
+  end
+
+  def test_copyright
+    assert_equal '2012 (c) loop', @doc.copyright
+  end
+
+  def test_title
+    assert_equal 'Timed Text DFPX', @doc.title
+  end
+
+  def test_subs_no_param
+    assert @doc.subtitle_stream.is_a?(Array), "Not a NodeSet!"
+  end
+
+  def test_subs_start_param
+    assert @doc.subtitle_stream(99999999.0).empty?
+    assert_equal 1, @doc.subtitle_stream(746.0).size
+  end
+
+  def test_subs_end_param
+    assert @doc.subtitle_stream(0.0, 0.0).empty?
+    assert_equal 1, @doc.subtitle_stream(746.63, 749.38).size
+  end
+
+
+end
